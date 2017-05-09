9c63b60cee275260a049be44af0d54f10bf770e1
settintotrieste <settintotrieste@gmail.com>
2017-05-09 21:35:43 +0800

    Tanganyika Tweaks + Superstate assimilation + Bugfixes/Tweaks
    Hungary revolution bug still present

--- CWE/common/defines.lua
+++ CWE/common/defines.lua
@@ -115,7 +115,7 @@ economy = {
 	FACTORY_PAYCHECKS_LEFTOVER_FACTOR = 0.9, -- % of how much we pay to the pops and capitalists, from the leftovers.
 	MAX_FACTORY_MONEY_SAVE = 5000,	-- how much money is stored maximum in a factory.
 	SMALL_DEBT_LIMIT = 5000, 
-	FACTORY_UPGRADE_EMPLOYEE_FACTOR = 0.95, -- determines how close to the employee limit we need to be before "upgrade all" will upgrade/expand a given factory (1 = 100%).
+	FACTORY_UPGRADE_EMPLOYEE_FACTOR = 0.75, -- determines how close to the employee limit we need to be before "upgrade all" will upgrade/expand a given factory (1 = 100%).
 	RGO_SUPPLY_DEMAND_FACTOR_HIRE_HI = 0.2,	-- how fast pops are Hired when there is a high demand
 	RGO_SUPPLY_DEMAND_FACTOR_HIRE_LO = 0.1,	-- how fast pops are Hired when there is a medium demand
 	RGO_SUPPLY_DEMAND_FACTOR_FIRE = 0.05,		-- how fast pops are Fired when there is a low demand
