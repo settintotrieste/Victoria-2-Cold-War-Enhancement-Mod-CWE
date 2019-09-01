d9c3cebb36883c056dacec0aa5ad246974e22801
settintotrieste <settintotrieste@users.noreply.github.com>
2019-09-01 20:34:31 +0800

    Flavour Update
    - From Mitusonator, set RGO_SUPPLY_DEMAND_FACTOR_FIRE = 0.00 to reduce RGO unemployment
    -Claim Rockall can only be done during the Cold War
    -Revised the Death of Mao to Deng Xiaoping's reforms event chain
    -Fixed the Federation of Arab Republics event (20% success chance)
    -Civil Service, Teacher's, Doctor's strikes
    -Supreme Court Judge selection event

--- CWE/common/defines.lua
+++ CWE/common/defines.lua
@@ -118,7 +118,7 @@ economy = {
 	FACTORY_UPGRADE_EMPLOYEE_FACTOR = 0.75, -- determines how close to the employee limit we need to be before "upgrade all" will upgrade/expand a given factory (1 = 100%).
 	RGO_SUPPLY_DEMAND_FACTOR_HIRE_HI = 0.2,	-- how fast pops are Hired when there is a high demand
 	RGO_SUPPLY_DEMAND_FACTOR_HIRE_LO = 0.1,	-- how fast pops are Hired when there is a medium demand
-	RGO_SUPPLY_DEMAND_FACTOR_FIRE = 0.05,		-- how fast pops are Fired when there is a low demand
+	RGO_SUPPLY_DEMAND_FACTOR_FIRE = 0.00,		-- how fast pops are Fired when there is a low demand
 	EMPLOYMENT_HIRE_LOWEST = 0.1,				-- we Hire pops no slower then x% of total required per day
 	EMPLOYMENT_FIRE_LOWEST = 0.1,				-- we Fire pops no slower then x% of total required per day
 	TRADE_CAP_LOW_LIMIT_LAND = 0.1, 				-- the lowest % the slider can go for land units
